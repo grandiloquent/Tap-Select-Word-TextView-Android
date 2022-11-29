1250af37b74e2ad2dffd3daea5d3197108f62487
grandiloquent <1785907865@qq.com>
2022-11-29 14:55:15 +0800

    更新

--- app/src/main/java/psycho/euphoria/tapselectword/MainActivity.java
+++ app/src/main/java/psycho/euphoria/tapselectword/MainActivity.java
@@ -82,41 +82,6 @@ public class MainActivity extends Activity {
         return (int) (range >>> 32);
     }
 
-    private String findWordForRightHanded(String str, int offset) { // when you touch ' ', this method returns left word.
-        if (str.length() == offset) {
-            offset--; // without this code, you will get exception when touching end of the text
-        }
-        if (str.charAt(offset) == ' ') {
-            offset--;
-        }
-        int startIndex = offset;
-        int endIndex = offset;
-        try {
-            while (str.charAt(startIndex) != ' ' && str.charAt(startIndex) != '\n') {
-                startIndex--;
-            }
-        } catch (StringIndexOutOfBoundsException e) {
-            startIndex = 0;
-        }
-        try {
-            while (str.charAt(endIndex) != ' ' && str.charAt(endIndex) != '\n') {
-                endIndex++;
-            }
-        } catch (StringIndexOutOfBoundsException e) {
-            endIndex = str.length();
-        }
-        // without this code, you will get 'here!' instead of 'here'
-        // if you use only english, just check whether this is alphabet,
-        // but 'I' use korean, so i use below algorithm to get clean word.
-        char last = str.charAt(endIndex - 1);
-        if (last == ',' || last == '.' ||
-                last == '!' || last == '?' ||
-                last == ':' || last == ';') {
-            endIndex--;
-        }
-        return str.substring(startIndex, endIndex);
-    }
-
     private long getCharRange(int offset) {
         final int textLength = mTextView.getText().length();
         if (offset + 1 < textLength) {
